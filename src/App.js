97689b2e5d62ca601a949cb3f2c05925ca166abc
KristinceSokolova <kristince.sokolova@gmail.com>
2022-12-26 19:59:00 +0100

    main clean up

--- src/App.js
+++ src/App.js
@@ -1,33 +1,4 @@
-import Student from "./components/Student"
-
 const App = () => {
-
-const name = "Mia Koluci"
-const age = 34
-const toShow = true
-const fruits = [`apple`, `android`, `banana`]
-
-const student = {
-  name,
-  age,
-  fruits,
+  return (<></>);
 }
-
-  return (
-    <div>
-    <div>Hello World</div>
-    <Student name={name} age={age} shouldTheComponentsShow={toShow}/>
-    </div>
-  );
-
-
-  
-}
-
-// function App() {
-//  return (
-//    <div>Hello World</div>
-//  );
-// }
-
 export default App;
